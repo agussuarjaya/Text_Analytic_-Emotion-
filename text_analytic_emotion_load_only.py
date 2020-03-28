cc75727f2b5f1e0be3336b1348f07f660d83afbf
diahputri90 <62321815+diahputri90@users.noreply.github.com>
2020-03-28 23:24:04 +0800

    Update text_analytic_emotion_load_only.py

--- text_analytic_emotion_load_only.py
+++ text_analytic_emotion_load_only.py
@@ -13,7 +13,7 @@ Original file is located at
  
   Copyright 2020  I Made Agus Dwi Suarjaya
                   Gede Ocha Dipa Ananda
-                  Author 3
+                  Ni Luh Putu Diah Putri Maheswari
 
   Description     : Try to analyze Tweets with TensorFlow and classify into 5 emotions (anger, happiness, sadness, love, fear)
   Dataset source  : https://raw.githubusercontent.com/meisaputri21/Indonesian-Twitter-Emotion-Dataset/master/Twitter_Emotion_Dataset.csv
